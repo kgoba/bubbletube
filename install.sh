ee7baff34e515ecc0a4453cdc0e4de01e9522f27
Karlis Goba <karlis.goba@gmail.com>
2016-02-15 23:42:06 +0000

    Added update scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
+cp bubbletube-update.service /etc/systemd/system/
 cp bubbletube.service /etc/systemd/system/
 cp pigpiod.service /etc/systemd/system
 
+systemctl enable bubbletube-update.service
 systemctl enable bubbletube.service
 systemctl enable pigpiod.service
 
