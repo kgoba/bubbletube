7d92862140a14b9a7379f642dfbc212a39e642f3
Karlis Goba <karlis.goba@gmail.com>
2016-02-15 15:32:37 +0000

    Added RGB task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import threading
 import logging
 import random
 import time
@@ -15,6 +16,28 @@ from hal import Status
 LOGFILE = None
 LOGLEVEL = logging.INFO
 
+def rgbTask(rgb):
+    names = ["R", "G", "B"]
+    color = [0.1, 0.3, 0.2]
+    while True:
+        idx = random.randint(0, 2)
+        dir = random.randint(0, 1) * 2 - 1
+        num = random.randint(100, 250)
+        #logging.info("Channel %s %d %.2f %.2f %.2f" % (names[idx], dir, color[0], color[1], color[2]))
+        for c in range(num):
+            color[idx] += 0.002 * dir
+            if color[idx] > 0.5:
+                color[idx] = 0.5
+                break
+            if color[idx] < 0.0:
+                color[idx] = 0.0
+                break
+            rgb.setLED(0, color[0], color[1], color[2])
+            rgb.setLED(1, color[0], color[1], color[2])
+            rgb.setLED(2, color[0], color[1], color[2])
+            time.sleep(0.02)
+    return
+
 def main(argv):
     logging.basicConfig(filename=LOGFILE,level=LOGLEVEL)
     logging.info("Starting up")
@@ -27,8 +50,20 @@ def main(argv):
     status = Status()
 
     logging.info("Devices initialized")
+    
+    t1 = threading.Thread(target = rgbTask, args = (rgb,))
+    logging.info("Threads initialized")
+
+    status.set(0)
+    t1.start()
     while True:
-        pass
+        states = sensors.read()
+        for i in range(6):
+            if states[i]:
+                status.set(i + 1)
+            else:
+                status.clear(i + 1)
+        time.sleep(0.2)
     return
     
 if __name__ == "__main__":
