bdb561ea67406677e0bc93108876f6f165dee805
Karlis Goba <karlis.goba@gmail.com>
2016-02-15 23:49:50 +0000

    Added system files (DHCP and UDEV)

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+cp dhcp-update-rules /etc/dhcp/dhclient-exit-hooks.d/update-src
+cp usb-update-rules /etc/udev/rules.d/11-usb-media-automount.rules
+
 cp bubbletube-update.service /etc/systemd/system/
 cp bubbletube.service /etc/systemd/system/
 cp pigpiod.service /etc/systemd/system
